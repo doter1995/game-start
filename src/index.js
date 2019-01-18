a06af6da9aa447e02b7a4925cde93a0162ad1692
weiDong ZHang <doter1995@qq.com>
2019-01-18 16:15:42 +0800

    [Add|Dong] add webpack simply demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import _ from 'lodash'
 function component() {
   var element = document.createElement('div');
 
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // 通过import导入 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   return element;
 }
 
