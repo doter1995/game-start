cfdaba9334a21a955770c59e0a33f41a09ba08b7
weiDong ZHang <doter1995@qq.com>
2019-01-18 23:26:47 +0800

    [Add|Dong] add webpack CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -10,6 +11,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "hello word"
-    })
+    }),
+    new CleanWebpackPlugin(["dist"])
   ]
 }
