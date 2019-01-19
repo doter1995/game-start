ccedf254739b20f41274b735a3c71bf981e225a4
weiDong ZHang <doter1995@qq.com>
2019-01-19 19:56:20 +0800

    [Add|Dong]add react render

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import _ from 'lodash'
 import './index.scss'
+import ReactDOM from 'react-dom'
 
-function component() {
-  var element = document.createElement('div')
+var element = document.createElement('div')
 
-  // 通过import导入
-  element.innerHTML = _.join(['Hello', 'webpack!!!!'], ' ')
-  element.classList.add('hello')
-  console.error('aaa')
-  return element
-}
-
-document.body.appendChild(component())
+// 通过import导入
+element.id = 'app'
+document.body.appendChild(element)
+ReactDOM.render('<div>aaa</div>', element)
