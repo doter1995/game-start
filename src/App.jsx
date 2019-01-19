d3d4faa4459bf5bc43c687200b4e05cb85f56a62
weiDong ZHang <doter1995@qq.com>
2019-01-19 23:24:07 +0800

    [Add|Dong]add antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import { Button } from 'antd'
 export default class App extends Component {
   render() {
     return (
       <div>
-        aaa
+        <Button type="primary">aa</Button>
       </div>
     )
   }
--- webpack.common.js
+++ webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: [/\.css$/, /\.scss$/],
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
