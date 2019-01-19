029d38fe74ce0a2d00d652c74e7475f58e378f45
weiDong ZHang <doter1995@qq.com>
2019-01-19 18:44:02 +0800

    [Add|Dong] add prettier config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import _ from 'lodash'
-import "./index.scss"
+import './index.scss'
 
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement('div')
 
-  // 通过import导入 
-  element.innerHTML = _.join(['Hello', 'webpack!!!!'], ' ');
-  element.classList.add('hello');
-  console.error("aaa");
-  return element;
+  // 通过import导入
+  element.innerHTML = _.join(['Hello', 'webpack!!!!'], ' ')
+  element.classList.add('hello')
+  console.error('aaa')
+  return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
