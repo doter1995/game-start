571fd75c24e6f2c866176cc1f86ecd42bf52554f
weiDong ZHang <doter1995@qq.com>
2019-01-20 17:58:24 +0800

    [Add|Dong] add babel

--- webpack.common.js
+++ webpack.common.js
@@ -1,27 +1,30 @@
-const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "hello word"
+      title: 'hello word',
     }),
-    new CleanWebpackPlugin(["dist"])
+    new CleanWebpackPlugin(['dist']),
   ],
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        "css-loader",
-        "sass-loader"
-      ]
-    }]
-  }
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+    ],
+  },
 }
